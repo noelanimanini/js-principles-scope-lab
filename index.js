eb44aadbc8ebe5271b5e262ee2f25cdbbbb11c27
crystal <noelani26@crystals-MacBook-Pro.local>
2021-01-12 16:43:22 -0800

    I think about this a lot

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+customerName = "bob" 
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+    return customerName
+}
+
+
+function setBestCustomer() {
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "Mary"    
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Ruby"
+}
